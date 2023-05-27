576f3e8cc33b6d486657ee83d666efa475534f0f
caio <caiovitordias1@gmail.com>
2023-05-27 12:46:07 -0300

    adicao de componetes de funcao

--- interfaceworldbeauty/src/pages/cadastro.jsx
+++ interfaceworldbeauty/src/pages/cadastro.jsx
@@ -1,26 +1,52 @@
+import { useState } from "react";
 import { Component } from "react";
 import Header from "../components/header";
 
-class Cadastro extends Component{
-    render(){
+// class Cadastro extends Component{
+//     render(){
 
-        return(
-            <div>
-                <Header />
-                <div className="divtitulos">
-                <h1>Cadastro de clientes!</h1>
-                </div>
-                <label>Nome</label>
-                <input></input>
-                <label>Gênero</label>
-                <input></input>
-                <label>CPF</label>
-                <input></input>
-                <label>Data de emissão</label>
-                <input></input>
+//         return(
+//             <div>
+//                 <Header />
+//                 <div className="divtitulos">
+//                 <h1>Cadastro de clientes!</h1>
+//                 </div>
+//                 <label>Nome</label>
+//                 <input></input>
+//                 <label>Gênero</label>
+//                 <input></input>
+//                 <label>CPF</label>
+//                 <input></input>
+//                 <label>Data de emissão</label>
+//                 <input></input>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Cadastro
+
+export default function Cadastro(){
+    const [nome, setNome] = useState("")
+    const [genero, setGenero] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [data, setData] = useState("")
+
+    
+    return(
+        <div>
+            <Header />
+            <div className="divtitulos">
+            <h1>Cadastro de clientes!</h1>
             </div>
-        )
-    }
+            <label>Nome</label>
+            <input></input>
+            <label>Gênero</label>
+            <input></input>
+            <label>CPF</label>
+            <input></input>
+            <label>Data de emissão</label>
+            <input></input>
+        </div>
+    )
 }
-
-export default Cadastro
--- interfaceworldbeauty/src/pages/listagem.jsx
+++ interfaceworldbeauty/src/pages/listagem.jsx
@@ -2,41 +2,75 @@ import { Component } from "react";
 import Header from "../components/header";
 import "../styles/listagem.css"
 
-class Listagem extends Component{
-    render(){
+// class Listagem extends Component{
+//     render(){
 
-        return(
-            <div>
-                <Header />
-                <h1>Listagem de clientes !</h1>
+//         return(
+//             <div>
+//                 <Header />
+//                 <h1>Listagem de clientes !</h1>
 
-                <h2>Qual o tipo da listagem desejada? </h2>
+//                 <h2>Qual o tipo da listagem desejada? </h2>
 
-                <div className="divmenu">
-                <label>Listar todos os clientes</label>
-                <button className="botao">Escolher</button>
+//                 <div className="divmenu">
+//                 <label>Listar todos os clientes</label>
+//                 <button className="botao">Escolher</button>
 
-                <label>Listar clientes por gênero</label>
-                <button className="botao">Escolher</button>
+//                 <label>Listar clientes por gênero</label>
+//                 <button className="botao">Escolher</button>
                 
-                <label>Listar 10 clientes que mais consumiram produtos</label>
-                <button className="botao">Escolher</button>
+//                 <label>Listar 10 clientes que mais consumiram produtos</label>
+//                 <button className="botao">Escolher</button>
                 
-                <label>Listar 10 clientes que menos consumiram produtos</label>
-                <button className="botao">Escolher</button>
+//                 <label>Listar 10 clientes que menos consumiram produtos</label>
+//                 <button className="botao">Escolher</button>
                 
-                <label>Listar os 5 clientes que mais consumiram em valor</label>
-                <button className="botao">Escolher</button>
+//                 <label>Listar os 5 clientes que mais consumiram em valor</label>
+//                 <button className="botao">Escolher</button>
                 
-                <label>Listar os produtos mais consumidos</label>
-                <button className="botao">Escolher</button>
+//                 <label>Listar os produtos mais consumidos</label>
+//                 <button className="botao">Escolher</button>
                 
-                <label>Listar os produtos mais consumidos por gênero</label>
-                <button className="botao">Escolher</button>
-                </div>
-            </div>
-        )
-    }
+//                 <label>Listar os produtos mais consumidos por gênero</label>
+//                 <button className="botao">Escolher</button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Listagem
+
+export default function Listagem(){
+    return (
+        <div>
+            <Header />
+            <h1>Listagem de clientes !</h1>
+
+            <h2>Qual o tipo da listagem desejada? </h2>
+
+        <div className="divmenu">
+            <label>Listar todos os clientes</label>
+            <button className="botao">Escolher</button>
+
+            <label>Listar clientes por gênero</label>
+            <button className="botao">Escolher</button>
+                
+            <label>Listar 10 clientes que mais consumiram produtos</label>
+            <button className="botao">Escolher</button>
+                
+            <label>Listar 10 clientes que menos consumiram produtos</label>
+            <button className="botao">Escolher</button>
+                
+            <label>Listar os 5 clientes que mais consumiram em valor</label>
+            <button className="botao">Escolher</button>
+                
+            <label>Listar os produtos mais consumidos</label>
+            <button className="botao">Escolher</button>
+                
+            <label>Listar os produtos mais consumidos por gênero</label>
+            <button className="botao">Escolher</button>
+        </div>
+        </div>
+    )
 }
-
-export default Listagem
