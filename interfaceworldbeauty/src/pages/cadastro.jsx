f31b8eaf612c121e79fb44a8fdb3b970577eebbd
caio <caiovitordias1@gmail.com>
2023-05-27 13:07:48 -0300

    integracao com backend

--- interfaceworldbeauty/src/pages/cadastro.jsx
+++ interfaceworldbeauty/src/pages/cadastro.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { Component } from "react";
 import Header from "../components/header";
@@ -33,20 +34,40 @@ export default function Cadastro(){
     const [data, setData] = useState("")
 
     
+    let objetoCliente = {
+        nome: nome,
+        genero: genero,
+        cpf: cpf,
+        data: data
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        axios.post("http://localhost:32832/cliente/cadastrar" , objetoCliente)
+        .then( res => {
+            let resposta = res.data
+            console.log(resposta)
+        })
+    }
+    
     return(
         <div>
             <Header />
             <div className="divtitulos">
-            <h1>Cadastro de clientes!</h1>
+                <h1>Cadastro de clientes!</h1>
             </div>
-            <label>Nome</label>
-            <input></input>
-            <label>Gênero</label>
-            <input></input>
-            <label>CPF</label>
-            <input></input>
-            <label>Data de emissão</label>
-            <input></input>
+            <form onSubmit={handleSubmit}>
+                <label>Nome</label>
+                <input onChange={(e) => setNome(e.target.value)}></input>
+                <label>Gênero</label>
+                <input onChange={(e) => setGenero(e.target.value)}></input>
+                <label>CPF</label>
+                <input onChange={(e) => setCpf(e.target.value)}></input>
+                <label>Data de emissão</label>
+                <input type="date" onChange={(e) => setData(e.target.value)}></input>
+                <button type="submit"> Cadastrar</button>
+            </form>
         </div>
     )
 }
